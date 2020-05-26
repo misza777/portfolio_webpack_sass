14bbe2b658f50f93693dddf2c4d193a43ad7d28d
Michal Nowak <miau.nowak@gmail.com>
2020-05-26 20:25:08 +0200

    button animation, menu visibility

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,50 @@
-import '../sass/index.scss';
+import "../sass/index.scss";
+
+//select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNavUl = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set initial State Of Menu
+let showMenu = false;
+
+const toggleMenu = () => {
+  //   console.log(this);
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNavUl.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+    showMenu = !showMenu;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNavUl.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+    showMenu = !showMenu;
+  }
+};
+menuBtn.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//   //   console.log(this);
+//   if (!showMenu) {
+//     menuBtn.classList.add("close");
+//     menu.classList.add("show");
+//     menuNavUl.classList.add("show");
+//     menuBranding.classList.add("show");
+//     navItems.forEach((item) => item.classList.add("show"));
+//     showMenu = !showMenu;
+//   } else {
+//     menuBtn.classList.remove("close");
+//     menu.classList.remove("show");
+//     menuNavUl.classList.remove("show");
+//     menuBranding.classList.remove("show");
+//     navItems.forEach((item) => item.classList.remove("show"));
+//     showMenu = !showMenu;
+//   }
+// }
